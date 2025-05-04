aa440497f788f3fcdb1b8be929fe4dedf01c6cc9
rafiahmedrifatt <rafiahmed0471@gmail.com>
2025-05-04 13:01:20 +0600

    important credential is hidden

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAWweuabuYinZ4KDN1NAGSVizitCarJjzM",
-    authDomain: "dragon-breaking-newss.firebaseapp.com",
-    projectId: "dragon-breaking-newss",
-    storageBucket: "dragon-breaking-newss.firebasestorage.app",
-    messagingSenderId: "42941315222",
-    appId: "1:42941315222:web:4ae1eb586e73429341eab7"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
 };
-
 
 const App = initializeApp(firebaseConfig);
 export const auth = getAuth();
